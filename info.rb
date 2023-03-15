8977aa44381d0c4ed15b8ae8c0e2d9703201d905
Elegard <whiteandy21321@gmail.com>
2023-03-15 23:02:10 +0300

    info about lesson

---
+++ info.rb
@@ -0,0 +1,39 @@
+отличие varchar от text в том что в первом можно задать длинну varchar(22)
+
+
+  <div class="mb-3">
+    <label>You'r barber </label>
+   <select name="barber"class="form-select">
+    <option <%=@barber=='Peter'? 'selected':''%> >Peter</option>
+    <option <%=@barber=='Jerry'? 'selected':''%> >Jerry</option>
+    <option   <%=@barber=='Gus'? 'selected':''(здесь происходит вычесление)%> >Gus</option>  значения будут приниматся по умолчанию
+   </select>
+  </div>
+
+
+
+
+Такой способ записи будем применять для создания базы данных и таблиц в ней при инициализации приложения:
+CREATE TABLE IF NOT EXISTS "Users" ("Id" INTEGER PRIMARY KEY AUTOINCREMENT, "Username" TEXT, "Phone" TEXT);
+
+
+
+
+
+ Sinatra есть специальная команда, которая запускается при инициализации приложения:
+configure do
+  # some code
+end
+
+
+подключение sqlite  require "sqlite3"
+
+
+  # создание таблицы
+configure do
+	@db=SQlite3::Database.new 'barbershop.db'
+	@db.execute"CREATE TABLE IF NOT EXISTS 
+	"Users"
+	("Id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	 "Username" TEXT, "Phone" TEXT,"datastamp" TEXT,"barber" TEXT,"color" TEXT);"
+end
